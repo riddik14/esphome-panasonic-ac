7b31765955ba6f0824065dc65bd7b50a76476cee
Domenico Ceccarelli <37450907+riddik14@users.noreply.github.com>
2025-07-14 14:38:57 +0200

    Update esppac.cpp

--- components/panasonic_ac/esppac.cpp
+++ components/panasonic_ac/esppac.cpp
@@ -19,14 +19,14 @@ climate::ClimateTraits PanasonicAC::traits() {
   traits.set_visual_temperature_step(TEMPERATURE_STEP);
 
   traits.set_supported_modes({climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_HEAT_COOL, climate::CLIMATE_MODE_COOL,
-                              climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_FAN_ONLY, climate::CLIMATE_MODE_DRY});
+                              climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_DRY});
 
   traits.set_supported_custom_fan_modes({"Automatic", "1", "2", "3", "4", "5"});
 
   traits.set_supported_swing_modes({climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_BOTH,
                                     climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL});
 
-  traits.set_supported_custom_presets({"Normal", "Powerful", "Quiet"});
+  traits.set_supported_custom_presets({"Normal"});
 
   return traits;
 }
